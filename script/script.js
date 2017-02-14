195d080aabad92fc599b9bccf538a6698ee2baae
Maxim Marchenko <slayer.jk@mail.ru>
2017-02-14 15:19:51 +0600

    Some adding/fixes for @mediaqueries; added jQuery code for top menu(hide/show).

---
+++ script/script.js
@@ -0,0 +1,19 @@
+/*global $, jQuery*/
+$(document).ready(function () {
+  'use strict';
+
+  $(".header-top-menu").show();
+  $(".toggle-header-menu").show();
+
+  $.fn.toggleTopMenu = function () {
+    $(".toggle-header-menu").click(function () {
+      $(".toggle-header-menu").toggleClass("toggle-header-menu__btn_active");
+      $(".header-top-menu").toggle('slide', 'slow');
+    });
+  };
+  
+  $.fn.toggleTopMenu();
+  
+  
+
+});
