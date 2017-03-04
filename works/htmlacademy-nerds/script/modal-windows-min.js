36605f5c9da63c7ff4b7f3b547bd6f58ec1fcb28
Maxim Marchenko <slayer.jk@mail.ru>
2017-03-04 16:30:45 +0600

    Added jS(jQuery) code for slider auto switching(no effects though) for all works.

--- works/htmlacademy-nerds/script/modal-windows-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){"use strict";$(".js-form").click(function(){event.preventDefault(),$(".overlay").show(),$(".modal-content_form").addClass("modal-content_show")}),$(".modal-content_close").click(function(){event.preventDefault(),$(".overlay").hide(),$(".modal-content_form").removeClass("modal-content_show")})});
--- works/htmlacademy-technomart/script/script.js
+++ works/htmlacademy-technomart/script/script.js
@@ -14,14 +14,37 @@ $(document).ready(function () {
   });
   
   $.fn.sliderSwitch = function () {
-    $('.items-slider__slides').fadeToggle(1000);
+    var
+      $curInputChecked = $('.items-list__input:checked'),
+      $nextInputChecked = $curInputChecked.next(),
+      $prevInputChecked = $curInputChecked.prev();
+    
+    $curInputChecked.removeAttr('checked');
+    if ($nextInputChecked.prop('type') === 'radio') {
+      $nextInputChecked.prop('checked', 'checked');
+    } else {
+      $nextInputChecked = $('.items-list__input').first().prop('checked', 'checked');
+    }
+    
+    $('.items-slider__icon-font-switch_left').on('click', function () {
+      if ($curInputChecked.prev().prop('type') === 'radio') {
+        $curInputChecked = $curInputChecked.prev().prop('checked', 'checked');
+      } else {
+        $curInputChecked = $('.items-list__input').last().prop('checked', 'checked');
+      }
+    });
+    
+    $('.items-slider__icon-font-switch_right').on('click', function () {
+      if ($curInputChecked.next().prop('type') === 'radio') {
+        $curInputChecked = $curInputChecked.next().prop('checked', 'checked');
+      } else {
+        $curInputChecked = $('.items-list__input').first().prop('checked', 'checked');
+      }
+    });
+    
   };
-
-  $(document).on('click', '.items-slider__switch , .items-slider__icon-font-switch', function () {
-    $(this).sliderSwitch();
-  });
-
-  setInterval($.fn.sliderSwitch, 4000);
+  
+  setInterval($.fn.sliderSwitch, 3000);
 
   // va-tests---------------------------------------------
   $('.logo_header, .loho_footer').click(function () {
