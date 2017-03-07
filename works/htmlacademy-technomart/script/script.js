25fa0ad54f38f1fa40cb336346f4e872b293d94d
Maxim Marchenko <slayer.jk@mail.ru>
2017-03-07 15:22:03 +0600

    Release of HTMLAcademy Technomart.

--- works/htmlacademy-technomart/script/script.js
+++ works/htmlacademy-technomart/script/script.js
@@ -4,7 +4,16 @@
 $(document).ready(function () {
   "use strict";
 
-  // slider-code----------------------------------------
+/*event.preventDefault() check for IE9*/
+  $.fn.eventPreventDefaultSafe = function () {
+    if (event.preventDefault) {
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  };
+  
+/* slider-code----------------------------------------*/
   $('.items-slider__icon-font-switch').hide();
   $('.items-list__item_slider').mouseover(function () {
     $('.items-slider__icon-font-switch').show();
@@ -16,19 +25,19 @@ $(document).ready(function () {
   $.fn.sliderSwitch = function () {
     var
       $curInputChecked = $('.items-list__input:checked'),
-      $nextInputChecked = $curInputChecked.next(),
-      $prevInputChecked = $curInputChecked.prev();
+      $nextInput = $curInputChecked.next(),
+      $prevInput = $curInputChecked.prev();
     
     $curInputChecked.removeAttr('checked');
-    if ($nextInputChecked.prop('type') === 'radio') {
-      $nextInputChecked.prop('checked', 'checked');
+    if ($nextInput.prop('type') === 'radio') {
+      $nextInput.prop('checked', 'checked');
     } else {
-      $nextInputChecked = $('.items-list__input').first().prop('checked', 'checked');
+      $nextInput = $('.items-list__input').first().prop('checked', 'checked');
     }
     
     $('.items-slider__icon-font-switch_left').on('click', function () {
       if ($curInputChecked.prev().prop('type') === 'radio') {
-        $curInputChecked = $curInputChecked.prev().prop('checked', 'checked');
+        $curInputChecked = $prevInput.prop('checked', 'checked');
       } else {
         $curInputChecked = $('.items-list__input').last().prop('checked', 'checked');
       }
@@ -36,23 +45,37 @@ $(document).ready(function () {
     
     $('.items-slider__icon-font-switch_right').on('click', function () {
       if ($curInputChecked.next().prop('type') === 'radio') {
-        $curInputChecked = $curInputChecked.next().prop('checked', 'checked');
+        $curInputChecked = $nextInput.prop('checked', 'checked');
       } else {
         $curInputChecked = $('.items-list__input').first().prop('checked', 'checked');
       }
     });
-    
   };
   
   setInterval($.fn.sliderSwitch, 3000);
-
-  // va-tests---------------------------------------------
-  $('.logo_header, .loho_footer').click(function () {
-    $(this).effect("bounce", {
-      times: 3
-    }, "slow");
-
+  
+/*Modal windows(write-us, map)*/
+  $('.about-us__button_contacts').on('click', function () {
+    $.fn.eventPreventDefaultSafe();
+    $('.overlay').show('rotate');
+    $('.write-us_index').fadeIn('slow');
   });
-  // -----------------------------------------------------  
+  
+  $('.contacts__map').on('click', function () {
+    $.fn.eventPreventDefaultSafe();
+    $('.overlay').show('rotate');
+    $('.map').fadeIn('slow');
+  });
+  
+  $('.modal__close-btn').on('click', function () {
+    $.fn.eventPreventDefaultSafe();
+    $('.overlay').hide();
+    $('.write-us_index, .map').fadeOut('slow');
+  });
+    
+/* va-tests---------------------------------------------*/
+  $('.logo_header-index, .logo_footer-index').click(function () {
+    $(this).effect("bounce", {times: 3}, "slow");
+  });
 
 });
